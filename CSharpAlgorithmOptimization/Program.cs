2d29575b4ac2fe1dad96c3a92918a4b5684d9bb3
Takuo Yoneda <shakeweb@gmail.com>
2017-10-04 16:29:13 +0900

    the first commit

---
+++ CSharpAlgorithmOptimization/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpAlgorithmOptimization
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(IsValid(6));
+
+			Console.ReadLine();
+		}
+
+		static List<int> Values;
+
+		static bool IsValid(int count)
+		{
+			Values = new List<int>() {1,2,3,4,5};
+
+			List<int> legend_hero_id_list = new List<int>();
+
+			bool is_duplicated = false;
+
+			foreach (var h in Values)
+			{
+				is_duplicated = false;
+				foreach (var i in legend_hero_id_list)
+				{
+					if (h == i)
+					{
+						is_duplicated = true;
+					}
+				}
+				if (is_duplicated == false)
+				{
+					legend_hero_id_list.Add(h);
+				}
+			}
+
+			//if (legend_hero_id_list.Count >= count)
+			//{
+			//	return true;
+			//}
+			return false;
+		}
+
+	}
+}
